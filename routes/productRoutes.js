0050d12e9d3f1572c8c76bced2d82c3da2406ced
MostofaAsik <mmostofaasik@gmail.com>
2025-03-02 15:30:54 +0600

    pagination apply

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,15 +2,20 @@ const express = require('express');
 const Product = require('../models/Product');
 const productRouter = express.Router();
 
+// Get all products
 productRouter.get('/api/products', async (req, res) => {
+    const { page = 1, limit = 10 } = req.query;
     try {
-        const products = await Product.find({});
+        const products = await Product.find({})
+            .skip((page - 1) * limit)
+            .limit(limit);
         res.status(200).json(products);
     } catch (err) {
         res.status(500).json({ message: 'Server error' });
     }
 });
 
+// Get a product by id
 productRouter.get('/api/products/:id', async (req, res) => {
     try {
         const product = await Product.findById(req.params.id);
@@ -20,6 +25,7 @@ productRouter.get('/api/products/:id', async (req, res) => {
     }
 });
 
+// Create a product
 productRouter.post('/api/products', async (req, res) => {
     const { name, price, category, stock, description } = req.body;
     console.log(req.body);
@@ -39,6 +45,7 @@ productRouter.post('/api/products', async (req, res) => {
     }
 });
 
+// Update a product
 productRouter.put('/api/products/:id', async (req, res) => {
     const { name, price, category, stock, description } = req.body;
     const updatedProduct = {
@@ -50,12 +57,14 @@ productRouter.put('/api/products/:id', async (req, res) => {
     };
     try {
         await Product.findByIdAndUpdate(req.params.id, updatedProduct);
+        if (!updatedProduct) return res.status(404).json({ message: 'Product not found' });
         res.status(200).json({ message: 'Product updated successfully' });
     } catch (err) {
         res.status(500).json({ message: 'Server error' });
     }
 });
 
+// Delete a product
 productRouter.delete('/api/products/:id', async (req, res) => {
     try {
         await Product.findByIdAndDelete(req.params.id);
