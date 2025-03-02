e53bbc60de584f484a17575faa83f245e3e6d162
MostofaAsik <mmostofaasik@gmail.com>
2025-03-02 15:12:52 +0600

    get all products api

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,14 @@ const express = require('express');
 const Product = require('../models/Product');
 const productRouter = express.Router();
 
-
+productRouter.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ message: 'Server error' });
+    }
+});
 
 productRouter.post('/api/products', async (req, res) => {
     const { name, price, category, stock, description } = req.body;
