b93136a8167bddab7b2d7a52528de6bb5d9be741
MostofaAsik <mmostofaasik@gmail.com>
2025-03-02 14:32:21 +0600

    mongodb connected

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 // const productRoutes = require('./routes/productRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000
@@ -10,7 +12,24 @@ const PORT = process.env.PORT || 5000
 app.use(cors());
 app.use(bodyParser.json());
 
+
+//routes
 // app.use(productRoutes);
+
+
+
+//mongodb connection
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rvjkksn.mongodb.net/ProductInventory?retryWrites=true&w=majority`);
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+    }
+};
+
+connectDB();
+
 app.get('/', (req, res) => {
     res.send('Product Inventory ApiS server is running');
 });
